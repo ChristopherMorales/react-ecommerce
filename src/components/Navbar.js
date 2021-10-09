37f07b57963e27e8308547e4eba2c1458366df0e
“Christoper <Christopher.morales@innovaresol.com>
2021-10-09 16:19:46 -0400

    Sidebar Toggle functionality

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -9,6 +9,9 @@ import { useProductsContext } from "../context/products_context";
 import { useUserContext } from "../context/user_context";
 
 const Nav = () => {
+
+  const {openSidebar} = useProductsContext()
+
   return (
     <NavContainer>
       <div className="nav-center">
@@ -17,7 +20,7 @@ const Nav = () => {
             
             <img src={logo} alt="comfy"></img>
           </Link>
-          <button type="button" className="nav-toggle">
+          <button type="button" className="nav-toggle" onClick={openSidebar}>
             
             <FaBars />
           </button>
